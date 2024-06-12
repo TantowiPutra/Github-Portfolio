40e7779a1718f138a212fc2434af4a96196b6532
tantowi.setiawan <tantowi.setiawan@binus.ac.id>
2024-06-12 23:46:27 +0700

    fixed image aspect ratio

--- src/components/common/ProjectsCard.js
+++ src/components/common/ProjectsCard.js
@@ -11,18 +11,20 @@ const ProjectsCard = ({ imgPath, title, description, techStack}) => {
 
     return (
         <div className="h-content w-full rounded shadow-lg hover:-translate-y-2 hover:shadow-2xl border-2 p-2 relative flex flex-col overflow-hidden fade-down">
-            <ImageContainer imgPath={imgPath} classes={"lg:h-[250px] sm:h-[400px] md:h-[300px] h-[350px] p-2 rounded w-full"} backgroundSize={"100% 100%"}/>
+            <ImageContainer 
+                imgPath={imgPath} 
+                classes={"p-2 rounded w-full"} 
+                backgroundSize={"100% 100%"}
+            />
             <div className="w-full h-full px-6 py-4 flex flex-col overflow-hidden">
                 <div className="font-bold xl:text-xl lg:text-lg text-base mb-2 overflow-auto mb-5">{title}</div>
-                <p className="xl:text-base lg:text-md text-sm text-white overflow-auto font-mono mb-10">
+                <p className="xl:text-base lg:text-md text-sm text-white overflow-auto font-mono">
                     {description}
                 </p>
 
-                <span className="mt-auto">
-                    <div className="max-w-[95%] flex w-full overflow-auto scrollbar-thin scrollbar-webkit">
-                        {techStackEntry}
-                    </div>
-                </span>
+                <div className="max-w-[95%] flex w-full overflow-auto scrollbar-thin scrollbar-webkit mt-auto h-content">
+                    {techStackEntry}
+                </div>
             </div>
         </div>
     )
