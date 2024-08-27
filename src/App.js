d75d98d6ef62c0b8a8e2c59083a25128f1958aa1
tantowi.setiawan <tantowi.setiawan@binus.ac.id>
2024-08-27 22:11:36 +0700

    FIXED ROUTING

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
-import Particle from './components/common/Particle.js';
-import Routes from 'utils/Routes';
-
+import { Route, HashRouter, Routes } from 'react-router-dom';
 import { SkeletonTheme } from 'react-loading-skeleton';
+
+import Content from './components/layout/Content';
+import Particle from './components/common/Particle';
+
+import NavbarData from '_data/Navbar';
+
 function App() {
+  const actNavbarData = NavbarData.filter((item) => item.isActive);
+
   return (
-    <>
-      <SkeletonTheme baseColor="#475569" highlightColor="#64748b">
-        <Particle /> 
-        <Routes />
-      </SkeletonTheme>
-    </>
+    <SkeletonTheme baseColor="#475569" highlightColor="#64748b">
+      <Particle />
+      <HashRouter>
+        <Routes>
+          {actNavbarData.map(({ id, href, section }) => (
+            <Route key={id} path={href} element={<Content href={href} section={section} />} />
+          ))}
+          {/* URL not found */}
+          <Route path="*" element={<Content currHref={actNavbarData[0]?.href} section={actNavbarData[0]?.section} />} />
+        </Routes>
+      </HashRouter>
+    </SkeletonTheme>
   );
 }
 
